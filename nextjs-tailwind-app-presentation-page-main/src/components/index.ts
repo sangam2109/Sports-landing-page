29745b685b3341d7fe15ddc882d8b3727ae2c1c4
SANGAM ARORA <98117962+sangam2109@users.noreply.github.com>
2024-01-20 18:03:14 +0530

    Delete nextjs-tailwind-app-presentation-page-main directory

--- nextjs-tailwind-app-presentation-page-main/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export * from "./navbar";
-export * from "./footer";
-export * from "./layout";
-export * from "./footer";
-export * from "./feature-card";
-export * from "./info-card";
-export * from "./fixed-plugin";
