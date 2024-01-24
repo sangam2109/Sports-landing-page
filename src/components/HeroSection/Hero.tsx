90868d09eca4b1654dbe432fe03e7f7d36d254f3
Inderpreet_Singh <singhinderpreet238@gmail.com>
2024-01-24 11:20:55 +0530

    - added main-page and changed images

--- src/components/HeroSection/Hero.tsx
+++ src/components/HeroSection/Hero.tsx
@@ -3,58 +3,19 @@
 import React from "react";
 import { Image } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
-import Android from "../../assets/Device Android FS (1).png";
+import Android from "../../assets/iphones.png";
 import { FaApple } from "react-icons/fa";
 
 const Hero: React.FC = () => {
   return (
-    <div className="bg-gray-900 py-16 px-4 h-[-webkit-fill-available]">
+    <div className="bg-gray-900 px-4 h-[-webkit-fill-available]">
       <div className="container mx-auto flex flex-col-reverse items-center md:flex-row  ">
         {/* Left Section */}
-        <div className="w-full md:w-1/2 mb-8 md:mb-0 md:pr-8">
-          <h1 className="text-4xl font-bold text-white mb-4">
-            Gndec Athletix
-          </h1>
-          <p className="text-gray-300 mb-4">
-            Elevate your athletic experience with the Gndec Athletix App. Track
-            your progress, set goals, and stay connected with the community.
-          </p>
-          {/* <div className="flex flex-col md:flex-row md:space-x-4"> */}
-                    <h3 className="font-bold  text-white opacity-90 mt-8">Get the app</h3>
-          <div className="flex flex-col justify-center items-center  md:flex-row gap-2 mt-2" >
-                    <Button className="w-[80vw] md:w-fit px-8 text-white max-w-[240px]" variant="bordered">
-                        <img width={20} src="https://demos.creative-tim.com/nextjs-tailwind-app-presentation-page/logos/logo-google.png" alt="" />
-                        Android
-                    </Button>
-                    <Button className="w-[80vw] md:w-fit px-8 text-white max-w-[240px] " color='primary' variant="shadow">
-                        {/* <img width={20} src={FaApple} alt="" /> */}
-                        <FaApple size={21}/>
-                        IOS
-                    </Button>
-          </div>
-          {/* </div> */}
-        </div>
-
-        {/* Right Section */}
-        <div className="w-full md:w-1/2 flex justify-center items-center">
-          {/* First Image */}
-          {/* <div className="p-4 mx-auto max-w-md mb-4">
-            {/* <Image
-              isBlurred
-              isZoomed
-              src={Android}
-              alt="App UI"
-              width={200}
-              height={200}
-              className="rounded"
-            /> */}
-          {/* </div>  */}
-
+        <div className="w-full pt-8 mt-8 md:w-1/2 flex justify-center items-center">
           {/* Second Image */}
-          <div className="p-4 mx-auto max-w-md">
+          <div className="mx-auto max-w-md">
             <Image
-              isBlurred
-              isZoomed
+              filter-blur-sm
               src={Android}
               alt="App UI"
               width={200}
@@ -63,6 +24,38 @@ const Hero: React.FC = () => {
             />
           </div>
         </div>
+
+         {/* Right Section */}
+        <div className="flex flex-col md:flex-col md:space-x-4">
+          <h3 className="font-bold text-4xl text-white text-xl opacity-90 mt-12">
+            Download the app
+          </h3>
+          <div className="flex flex-col justify-center items-center  md:flex-row gap-2 mt-4">
+            <Button
+              className="w-[80vw] md:w-fit px-8 text-white max-w-[240px]"
+              variant="bordered"
+            >
+              <img
+                width={20}
+                src="https://demos.creative-tim.com/nextjs-tailwind-app-presentation-page/logos/logo-google.png"
+                alt=""
+              />
+              Android
+            </Button>
+            <Button
+              className="w-[80vw] md:w-fit px-8 text-white max-w-[240px] "
+              color="primary"
+              variant="shadow"
+            >
+              {/* <img width={20} src={FaApple} alt="" /> */}
+              <FaApple size={21} />
+              IOS
+            </Button>
+          </div>
+          {/* </div> */}
+        </div>
+
+       
       </div>
     </div>
   );
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import background from "../../assets/background.png";
+import { Image } from "@nextui-org/react";
+// import { Button } from "@nextui-org/react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="bg-gray-900 relative wfull h-auto overflow-hidden">
+      <div className="aspect-w-16 aspect-h-9">
+        <Image
+          src={background}
+          alt="Background"
+          className="w-[320vh] h-[80vh] md:h-[80vh] object-cover opacity-25 filter blur-sm brightness-105 contrast-125 "
+        />
+      </div>
+      <div className="absolute inset-0 flex flex-row items-center justify-center text-white text-center md:flex flex-col">
+        <div className="w-full h-auto">
+          <h1 className="text-6xl m-4 font-bold md:text-8xl"> GNDEC ATHLETIX</h1>
+        </div>
+        <div className="text-xl m-8 pt-8 md:text-4xl ">
+          Elevate your athletic experience with the Gndec Athletix App. Track
+          your progress, set goals, and stay connected with the community.
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
