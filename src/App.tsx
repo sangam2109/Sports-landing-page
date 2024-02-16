ea8a4daa1444b5848200d1a9eca57c18e3a57e86
Inderpreet_Singh <singhinderpreet238@gmail.com>
2024-02-16 14:30:26 +0530

    tabs_added

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import CustomNavbar from "./components/Navbar/CustomNavbar"
 import Home from "./components/Home/Home";
 import Events from "./components/Events/EventsList";
 import { useState } from "react";
+import EventPage from "./components/Events/EventPage";
 
 function App() {
   const [SelectedCARD, setSelectedCard] = useState();
@@ -16,7 +17,7 @@ function App() {
       <Home />
       <Hero />
       <Events SelectedCARD={SelectedCARD}/>
-
+      <EventPage />
       <DevTeam />
       <FAQ />
       <Footer setSelectedCard={setSelectedCard} />
